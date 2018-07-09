9bf216920a53e8ae709e6248121d12c8b3f599c5
20171105112 <903056683@qq.com>
2018-07-09 14:12:55 +0800

    Initial Commit

---
+++ 2018.7.9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2018.7.9
+//
+//  Created by 黄宏 on 2018/7/9.
+//  Copyright © 2018年 黄宏. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
